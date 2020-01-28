3fd81fdf5bfd8c2a268dee7b7253725268ab413d
Jammet Rémy <rjammet07@gmail.com>
2020-01-28 13:44:27 +0000

    Update systemDatabase.php

--- PHP - HTML - JS/systemDatabase.php
+++ PHP - HTML - JS/systemDatabase.php
@@ -1,3 +1,13 @@
 <?php
+    define('HOST', 'mistorydyvadmin.mysql.db');
+    define('DB_NAME', '');
+    define('USER', '');
+    define('PASS', '7');
 
+    try{
+        $db = new PDO("mysql:host=" . HOST . ";dbname=" . DB_NAME, USER, PASS);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }catch (PDOException $error){
+        echo $error;
+    }
 ?>
